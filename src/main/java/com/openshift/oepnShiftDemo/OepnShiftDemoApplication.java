2413264a1f1ea7ef052900f8504600b12bdf9b5a
Nayan Singh <nayansingh196@gmail.com>
2023-03-04 22:51:12 +0530

    First Commit

---
+++ src/main/java/com/openshift/oepnShiftDemo/OepnShiftDemoApplication.java
@@ -0,0 +1,13 @@
+package com.openshift.oepnShiftDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OepnShiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OepnShiftDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openshift/oepnShiftDemo/OepnShiftDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.oepnShiftDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OepnShiftDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
